66ac9092bc2b414e1939131bbc3204fb7fc108d3
masha.kul <m.kulikova@itransition.com>
2021-10-10 15:30:30 +0300

    Add wishlists CRUD

---
+++ src/components/Wishlists/WishlistField/WishlistField.tsx
@@ -0,0 +1,78 @@
+import React, { useCallback, useState } from "react";
+import { Button, TextField, Paper, Dialog, DialogTitle, DialogActions } from "@material-ui/core";
+import { Delete, Edit, RotateLeft, Save, Visibility } from "@material-ui/icons";
+import styles from "./WishlistField.module.scss";
+import { useMutation } from "@apollo/client";
+import { CREATE_WISHLIST, DELETE_WISHLIST, UPDATE_WISHLIST } from "../../../queries/wishlist";
+import { NavLink } from "react-router-dom";
+import { paths } from "../../../constants/routes";
+
+export interface IWishlistProps {
+  id?: string;
+  name?: string;
+  reset: () => void;
+}
+
+const WishlistField: React.FC<IWishlistProps> = ({ id, name = '', reset }) => {
+  const [createWishlist] = useMutation(CREATE_WISHLIST);
+  const [updateWishlist] = useMutation(UPDATE_WISHLIST);
+  const [deleteWishlist] = useMutation(DELETE_WISHLIST);
+  const [isEditable, setIsEditable] = useState<boolean>(!id);
+  const [isOpenConfirmation, setIsOpenConfirmation] = useState<boolean>(false);
+  const [value, setValue] = useState<string>(name);
+
+  const toggleEditForm = useCallback(async () => {
+    if (id) {
+      await updateWishlist({ variables: { name: value, id } });
+    } else {
+      await createWishlist({ variables: { name: value } });
+    }
+
+    reset();
+    setIsEditable((value) => !value);
+  }, [createWishlist, updateWishlist, value, id, reset]);
+  const toggleConfirmation = useCallback(() => setIsOpenConfirmation((value) => !value), []);
+  const handleChange = useCallback(({ target }) => setValue(target.value), []);
+  const handleDelete = useCallback(async () => {
+    await deleteWishlist({ variables: { id } });
+    reset();
+    toggleConfirmation();
+  }, [deleteWishlist, id, reset, toggleConfirmation]);
+
+  return (
+    <>
+      <Dialog open={isOpenConfirmation} onClose={toggleConfirmation}>
+        <DialogTitle>{`Are you sure you want to delete the "${name}" wishlist?`}</DialogTitle>
+        <DialogActions>
+          <Button onClick={toggleConfirmation} startIcon={<RotateLeft />}>Cancel</Button>
+          <Button onClick={handleDelete} startIcon={<Delete />} color="error">Delete</Button>
+        </DialogActions>
+      </Dialog>
+      <Paper className={styles.wrapper}>
+        <div className={styles.name}>
+          {isEditable ? <TextField value={value} onChange={handleChange} variant="standard" /> : name}
+        </div>
+        <Button className={styles.btn} variant="outlined" startIcon={isEditable ? <Save /> : <Edit />} onClick={toggleEditForm}>
+          {isEditable ? "Save" : "Rename"}
+        </Button>
+        <Button className={styles.btn}  variant="outlined" onClick={toggleConfirmation} startIcon={<Delete />} color="error">
+          Delete
+        </Button>
+        {id && (
+          <Button
+            className={styles.btn}
+            variant="outlined"
+            onClick={toggleConfirmation}
+            startIcon={<Visibility />}
+            component={NavLink}
+            to={paths.wishlist.replace(':id', id.toString())}
+          >
+            Show
+          </Button>
+        )}
+      </Paper>
+    </>
+  );
+}
+
+export default WishlistField;
--- src/components/Wishlists/Wishlists.tsx
+++ src/components/Wishlists/Wishlists.tsx
@@ -1,6 +1,51 @@
-const Wishlists = () => {
+import { useQuery } from "@apollo/client";
+import { GET_WISHLISTS } from "../../queries/wishlist";
+import WishlistField from "./WishlistField/WishlistField";
+import React, { useCallback, useState } from "react";
+import { Button, Grid, Paper, Typography } from "@material-ui/core";
+import { Add, RotateLeft } from "@material-ui/icons";
+import styles from "./Wishlists.module.scss";
+
+interface IWishlist {
+  _id: string;
+  name: string;
+  movies: number[];
+}
+
+interface IWishlistsQuery {
+  wishlists: IWishlist[];
+}
+
+const Wishlists: React.FC = () => {
+  const { data, refetch } = useQuery<IWishlistsQuery>(GET_WISHLISTS);
+  const [isCreation, setIsCreation] = useState<boolean>(false);
+  const toggleCreation = useCallback(() => setIsCreation((value) => !value), [])
+  const reset = useCallback(async () => {
+    await refetch();
+    setIsCreation(false);
+  }, [refetch]);
+
   return (
-    <div>Wishlists</div>
+    <div>
+      <Paper className={styles.header}>
+        <Typography variant="h5">Wishlists</Typography>
+        <Button variant="outlined" startIcon={isCreation ? <RotateLeft /> :  <Add />} onClick={toggleCreation} >
+          {isCreation ? "Cancel" : "Add wishlist"}
+        </Button>
+      </Paper>
+      <Grid container spacing={"10"}>
+        {isCreation && (
+          <Grid item xs={12} sm={6} md={4}>
+            <WishlistField reset={reset} />
+          </Grid>
+        )}
+        {data?.wishlists.map(({ _id, ...props }) => (
+          <Grid key={_id} item xs={12} sm={6} md={4}>
+            <WishlistField reset={reset} id={_id} {...props} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
   );
 }
 
